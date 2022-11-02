bb213d6fb9584cd7e4a625d05cd4050e8bebff71
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2022-11-02 19:35:33 -0300

    Adicionando app intalados no settings

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'main',
+    'base',
+    'usuario'
 ]
 
 MIDDLEWARE = [

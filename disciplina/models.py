28e6dc7ed22c8e21115722890cb421ade9e3fec0
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2022-11-07 00:52:47 -0300

    Add validators

--- disciplina/models.py
+++ disciplina/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from professor.models import Professor
-
+from base.validators import validateNoDigits
 class Disciplina(models.Model):
-    nome_disciplina = models.CharField(max_length=50, blank=False, null=True)
+    nome_disciplina = models.CharField(max_length=50, blank=False, null=True, validators=[validateNoDigits])
     nome_professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     ano = models.DateField()
 
--- professor/models.py
+++ professor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from usuario.models import Usuario
+from base.validators import validateNoDigits
 # Create your models here.
 class Professor(models.Model):
     choices_grau= (
@@ -9,7 +10,7 @@ class Professor(models.Model):
     )
     nome_professor = models.ForeignKey(Usuario, on_delete=models.CASCADE, limit_choices_to={'departamento': 'Professsor'})
     grau_formacao = models.CharField(max_length=10, blank=False, null=True, choices=choices_grau)
-    formacao = models.CharField(max_length=20, blank=False, null=True)
+    formacao = models.CharField(max_length=20, blank=False, null=True, validators=[validateNoDigits])
 
     def __str__(self) -> str:
         return str(self.nome_professor)

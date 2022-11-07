8754c002394ef925ea97e2a413e09a24bcc9fe8e
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2022-11-06 23:52:38 -0300

    ajustes

--- aluno/models.py
+++ aluno/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from usuario.models import Usuario
 
 class Aluno(models.Model):
-    nome_aluno = models.ForeignKey(Usuario, on_delete=models.CASCADE, limit_choices_to={'departamento': 3})
+    nome_aluno = models.ForeignKey(Usuario, on_delete=models.CASCADE, limit_choices_to={'departamento': 'Aluno'})
 
 
     def __str__(self) -> str:
-        return self.nome_aluno
+        return str(self.nome_aluno)
